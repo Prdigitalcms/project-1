cc2b01cdb59ef33439e82c586e44614c41bcc78d
Prdigitalcms <prdigitalcms@gmail.com>
2025-03-21 18:44:29 +0530

    udpate

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -101,7 +101,7 @@ export default function Pricing() {
 
       {/* Pricing Cards */}
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 w-full h-full bg-400">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-14 mx-auto">
           {pricingPlans.map((plan, index) => (
             <motion.div
               key={plan.name}
